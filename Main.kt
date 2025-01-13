671af0eff50704cbcd75ed457740bd3869358e5b
OnlyCoiner <onlycoiner@gmail.com>
2025-01-13 08:11:41 -0300

    Include the first example

---
+++ Main.kt
@@ -0,0 +1,9 @@
+// $kotlinc Main.kt -include-runtime -d Main.jar
+// $java -jar Main.kt
+// You can also install some plugins and use Control + Option + n to run code.
+
+fun main() {
+  println("Hello world!")
+}
+
+
